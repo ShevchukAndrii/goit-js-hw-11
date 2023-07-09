579e139ac42b33f0a8263e997c44d556fc3573b0
ShevchukAndrii <andrefanchik@gmail.com>
2023-07-10 01:22:20 +0300

    на поверку

---
+++ src/index.js
@@ -0,0 +1,147 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchPhotos } from "./api.js";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+let simpleLightBox;
+var query = "";
+var page = 1;
+const perPage = 40;
+
+const searchFormEl = document.querySelector(".search-form");
+const galleryListEl = document.querySelector(".gallery");
+const loadMoreBtnEl = document.querySelector('.load-more');
+const inputEl = searchFormEl.firstElementChild;
+
+
+function createGallery(photos) {
+    const markup = photos
+        .map(photo => {
+            const {
+                largeImageURL,
+                webformatURL,
+                tags,
+                likes,
+                views,
+                comments,
+                downloads,
+            } = photo;
+            return `<div class="photo-card">
+            <a class="gallery-link" href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${downloads}
+    </p>
+  </div>
+</a></div>`;
+        })
+        .join('');
+
+    return markup;
+}
+
+//   // Цей код дозволяє автоматично прокручувати сторінку на висоту 2 карток галереї, коли вона завантажується
+//   const { height: cardHeight } = document
+//     .querySelector('.gallery')
+//     .firstElementChild.getBoundingClientRect();
+
+//   window.scrollBy({
+//     top: cardHeight * 2,
+//     behavior: 'smooth',
+//   });
+// }
+
+
+function handleSearchFormOnSubmit(event) {
+    event.preventDefault();
+    query = inputEl.value.trim();
+    page = 1;
+    if (!query) {
+    Notify.warning(
+        'The search string cannot be empty.', {
+            width: '300px',
+            position: 'center-center',
+            timeout: 3000,
+        }
+    );
+    return;
+    }
+    fetchPhotos(query, page, perPage).then(data => {
+        console.log(data);
+        if (data.totalHits === 0) {
+            Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.', {
+            width: '300px',
+            position: 'center-center',
+            timeout: 3000,
+            });
+        } else {
+            galleryListEl.innerHTML = createGallery(data.hits);
+            simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+            Notify.success(`Hooray! We found ${data.totalHits} images.`, {
+            width: '300px',
+            position: 'center-center',
+            timeout: 3000,
+            });
+            if (data.totalHits > perPage) {
+            loadMoreBtnEl.classList.remove("is-hidden")
+            }
+            
+           
+      }
+    }).catch(onFetchError).finally(() => {
+        searchFormEl.reset();
+    })
+
+
+}
+
+function handleMoreBtnClick() {
+    page += 1;
+    simpleLightBox.destroy();
+    fetchPhotos(query, page, perPage).then(data => {
+        amountOfPages = Math.ceil(data.totalHits / perPage);
+        if (page === amountOfPages) {
+            loadMoreBtnEl.classList.add('is-hidden');
+            Notify.failure(
+          'Sorry, there are no more photos.', {
+            width: '300px',
+            position: 'center-center',
+            timeout: 3000,
+            });
+            
+    }
+    galleryListEl.insertAdjacentHTML('beforeend', createGallery(data.hits));
+    simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+      }
+    ).catch(onFetchError)
+
+}
+
+
+
+
+
+
+searchFormEl.addEventListener("submit", handleSearchFormOnSubmit);
+loadMoreBtnEl.addEventListener("click", handleMoreBtnClick)
+
+
+function onFetchError(error) {
+    Notify.failure('Oops! Something went wrong! Try reloading the page!', {
+        position: 'center-center',
+        timeout: 3000,
+        width: '600px',
+        fontSize: '24px',
+    });
+};
