b07a5207b8be835df32288db0d31464fad79e4b3
ShevchukAndrii <andrefanchik@gmail.com>
2023-07-10 23:56:13 +0300

    оооо

--- src/api.js
+++ src/api.js
@@ -1,13 +1,8 @@
-
+import axios from 'axios';
 const key = "11934457-4993f38e20ffc9cd54235e1b7";
 const baseUrl = "https://pixabay.com/api/";
 
-export function fetchPhotos(query, page, perPage) {
-    return fetch(`${baseUrl}?key=${key}&q=${query}&image_type=photo&orientation=horizontal&safesearch=false&page=${page}&per_page=${perPage}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        })
-}
+export async function fetchPhotos(query, page, perPage) {
+    const response = await axios.get(`${baseUrl}?key=${key}&q=${query}&image_type=photo&orientation=horizontal&safesearch=false&page=${page}&per_page=${perPage}`)
+        return response.data;
+        }
